d56d886df6b73a76a1a8f45fa1e8b16e7f0295b4
tmortx7 <99x7eo@gmail.com>
2023-04-07 19:51:17 -0600

    bottom nav

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import { type FC } from "react"
 import { Header } from "./Header"
+import { BottomNav } from "./BottomNav"
 
 type AppProps = {
   children: React.ReactNode
@@ -10,6 +11,7 @@ export const Layout:FC<AppProps> = ({children}) =>{
     <div className="flex flex-col w-full h-[100vh]">
       <Header />
       {children}
+      <BottomNav/>
     </div>
   )
 }
