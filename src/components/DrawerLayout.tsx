a94d7357d635e5c2812e8f4eda83a2caa51883e0
tmortx7 <99x7eo@gmail.com>
2023-04-09 19:45:32 -0600

    login drawer

--- src/components/DrawerLayout.tsx
+++ src/components/DrawerLayout.tsx
@@ -1,16 +1,22 @@
-import Link from 'next/link';
 import React from "react";
+import { useAuth, SignedIn, SignedOut } from "@clerk/nextjs";
+import router from "next/router";
 import { useSessionStorage } from "usehooks-ts";
+import { VscSignIn, VscSignOut } from "react-icons/vsc";
+import COC from "../../public/coc-logo-simplify.svg";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const DrawerLayout = ({ children }: Props) => {
+
+
   //initialize state here. we use a key and a default state
   const [open, setOpen] = useSessionStorage("drawer", false);
+  const { signOut } = useAuth();
   return (
-    <div className="drawer">
+    <div className="drawer drawer-end">
       <input
         id="app-drawer"
         type="checkbox"
@@ -26,14 +32,32 @@ const DrawerLayout = ({ children }: Props) => {
           // add a onClick handler here to close the drawer
           onClick={() => setOpen(false)}
         ></label>
-        <ul className="menu p-2 overflow-y-auto w-[150px] bg-base-100">
-          <li>
-            <Link href="/home">Home</Link>
-          </li>
-          <li>
-            <a>Sidebar</a>
-          </li>
-        </ul>
+        <div className="w-[110px] overflow-y-auto bg-white">
+          <div className="flex flex-col items-center">
+            <COC className="mt-4 h-[40px]" />
+            <div className="divider"></div>
+            <div className="fixed bottom-0 mb-[20px] w-full">
+              <div className="flex flex-row justify-center">
+                 <SignedOut>
+                  <button onClick={() => void router.push("/signin")}>
+                    <VscSignIn size={30} />
+                  </button>
+                </SignedOut>
+                <SignedIn>
+                  <button
+                    onClick={() => {
+                      setOpen(false);
+                      void signOut();
+
+                    }}
+                  >
+                    <VscSignOut size={30} />
+                  </button>
+                </SignedIn>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,54 +1,15 @@
 import COC from "../../public/coc-logo.svg";
-import router from "next/router";
-import { useAuth, SignedIn, SignedOut, useUser } from "@clerk/nextjs";
 
 export const Header = () => {
-  const { signOut } = useAuth();
-  const {user} = useUser();
+
 
   return (
     <div className="flex flex-row bg-slate-700  items-center">
       <div className="flex basis-3/4 flex-row gap-4">
-        <COC className="h-[30px] p-1" />
+        <COC className="h-[50px] p-2" />
       </div>
       <div className="flex basis-1/4 flex-row justify-end">
-        <SignedOut>
-          <button
-            onClick={() => void router.push("/signin")}
-            className="btn btn-success text-slate-100 btn-sm  mr-2 p-2"
-          >
-            sign-in
-          </button>
-        </SignedOut>
-        <SignedIn>
-          <div className="dropdown-end dropdown">
-            <label tabIndex={0} className="">
-              <div className="">
-                <button className="text-slate-50 mr-4 ">{user?.lastName}</button>
-              </div>
-            </label>
-            <ul
-              tabIndex={0}
-              className="dropdown-content menu rounded-box mt-4 w-52 bg-white text-black shadow"
-            >
-              <li className="hover:bg-gray-50 hover:text-black ">
-                <button onClick={() => void router.push("/user")}>
-                  Profile
-                </button>
-              </li>
-              <li className="hover:bg-gray-50 hover:text-black">
-                <button
-                  onClick={() => {
-                    void signOut()
-                    void router.push("/");
-                  }}
-                >
-                  Logout
-                </button>
-              </li>
-            </ul>
-          </div>
-        </SignedIn>
+
       </div>
     </div>
   );
